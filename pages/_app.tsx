8579eca0cfa64f66c46b7d54cd8556459b2905d5
Roch Armengaud <armengaud.roch@live.fr>
2023-04-19 18:35:32 +0200

    Input component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import type { AppProps } from 'next/app';
 import { Layout } from '@/components/Layout';
 import '@/styles/globals.css';
-import { Modal } from '@/components/Modal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Modal isOpen title="Test Modal" actionLabel="Submit" />
       <Layout>
         <Component {...pageProps} />
       </Layout>
