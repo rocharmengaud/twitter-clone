983f45794c1ca29779c333a3d3956cfeec5e148e
Roch Armengaud <armengaud.roch@live.fr>
2023-04-25 17:53:48 +0200

    Who to follow section wip

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -1,9 +1,28 @@
+import useUsers from '@/hooks/useUsers';
+import { Avatar } from '../Avatar';
+
 export const FollowBar = () => {
+  const { data: users = [] } = useUsers();
+
+  if (users.length === 0) {
+    return null;
+  }
+
   return (
     <div className="lg:block hidden px-6 py-4">
       <div className="bg-neutral-800 rounded-xl p-4">
         <h2 className="text-xl font-semibold text-white">Who to follow</h2>
-        <div className="flex flex-col gap-6 mt-4">{/* USER LIST */}</div>
+        <div className="flex flex-col gap-6 mt-4">
+          {users.map((user: Record<string, any>) => (
+            <div className="flex flex-row gap-4" key={user.id}>
+              <Avatar userId={user.id} />
+              <div className="flex flex-col">
+                <p className="text-sm font-semibold text-white">{user.name}</p>
+                <p className="text-neutral-400 text-sm">@{user.username}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
