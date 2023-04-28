dd0bb84bef97f93944da62fa6b7171f52111071d
Roch Armengaud <armengaud.roch@live.fr>
2023-04-28 18:18:02 +0200

    Load/Post tweets functional

---
+++ components/posts/PostItem.tsx
@@ -0,0 +1,81 @@
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { useLoginModal } from '@/hooks/useLoginModal';
+import { formatDistanceToNowStrict } from 'date-fns';
+import { useRouter } from 'next/router';
+import React, { useCallback, useMemo } from 'react';
+import { Avatar } from '../Avatar';
+import { AiOutlineHeart, AiOutlineMessage } from 'react-icons/ai';
+
+interface PostItemProps {
+  data: Record<string, any>;
+  userId?: string;
+}
+
+// data is our post
+export const PostItem: React.FC<PostItemProps> = ({ data, userId }) => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const { data: currentUser } = useCurrentUser();
+
+  const goToUser = useCallback(
+    (event: any) => {
+      // Overwriting the global onClick of the parents
+      event.stopPropagation();
+
+      router.push(`/users/${data.user.id}`);
+    },
+    [router, data.user.id]
+  );
+
+  const goToPost = useCallback(() => {
+    router.push(`/posts/${data.id}`);
+  }, [router, data.id]);
+
+  const onLike = useCallback(
+    (event: any) => {
+      event.stopPropagation();
+
+      loginModal.onOpen();
+    },
+    [loginModal]
+  );
+
+  const createdAt = useMemo(() => {
+    if (!data?.createdAt) {
+      return null;
+    }
+
+    return formatDistanceToNowStrict(new Date(data.createdAt));
+  }, [data?.createdAt]);
+
+  return (
+    <div onClick={goToPost} className="border-b-[1px] border-neutral-800 p-5 cursor-pointer hover:bg-neutral-900 transition">
+      <div className="flex flex-row items-start gap-3">
+        <Avatar userId={data.user.id} />
+        <div>
+          <div className="flex flex-row items-center gap-2">
+            <p onClick={goToUser} className="hover:underline font-semibold text-white cursor-pointer">
+              {data.user.name}
+            </p>
+            <span onClick={goToUser} className="text-neutral-500 hover:underline md:block hidden cursor-pointer">
+              @{data.user.username}
+            </span>
+            <span className="text-neutral-500 text-sm">{createdAt} ago</span>
+          </div>
+          <div className="mt-1 text-white">{data.body}</div>
+          <div className="flex flex-row items-center gap-10 mt-3">
+            <div className="text-neutral-500 hover:text-sky-500 flex flex-row items-center gap-2 transition cursor-pointer">
+              <AiOutlineMessage size={20} />
+              <p>{data.comments?.length || 0}</p>
+            </div>
+            <div onClick={onLike} className="text-neutral-500 hover:text-red-500 flex flex-row items-center gap-2 transition cursor-pointer">
+              <AiOutlineHeart size={20} />
+              <p>{data.comments?.length || 0}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
