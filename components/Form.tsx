7ef973310732935b70bc7a44a701f55a514d4239
Roch Armengaud <armengaud.roch@live.fr>
2023-04-28 17:49:20 +0200

    Posting a tweet functional network res

--- components/Form.tsx
+++ components/Form.tsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import React, { useCallback, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { Button } from './Button';
+import { Avatar } from './Avatar';
 
 interface FormProps {
   placeholder: string;
@@ -42,13 +43,34 @@ export const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) =>
 
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
-      <div className="py-8">
-        <h1 className="mb-4 text-2xl font-bold text-center text-white">Welcome to twitter-clone</h1>
-        <div className="flex flex-row items-center justify-center gap-4">
-          <Button label="Login" onClick={loginModal.onOpen} />
-          <Button label="Register" onClick={registerModal.onOpen} secondary />
+      {currentUser ? (
+        <div className="flex flex-row gap-4">
+          <div>
+            <Avatar userId={currentUser?.id} />
+          </div>
+          <div className="w-full">
+            <textarea
+              disabled={isLoading}
+              onChange={(e) => setBody(e.target.value)}
+              value={body}
+              placeholder={placeholder}
+              className="disabled:opacity-80 peer resize-none mt-3 w-full bg-black ring-0 outline-none text-[20px] placeholder-neutral-500 text-white"
+            ></textarea>
+            <hr className="opacity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transition" />
+            <div className="flex flex-row justify-end mt-4">
+              <Button label="Tweet" disabled={isLoading || !body} onClick={onSubmit} />
+            </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="py-8">
+          <h1 className="mb-4 text-2xl font-bold text-center text-white">Welcome to twitter-clone</h1>
+          <div className="flex flex-row items-center justify-center gap-4">
+            <Button label="Login" onClick={loginModal.onOpen} />
+            <Button label="Register" onClick={registerModal.onOpen} secondary />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
