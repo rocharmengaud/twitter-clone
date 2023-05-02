d0deb809549246489f5cb81e7ebaaa4d6eccbb11
Roch Armengaud <armengaud.roch@live.fr>
2023-05-02 17:49:40 +0200

    comments functionnality working

--- components/Form.tsx
+++ components/Form.tsx
@@ -7,6 +7,7 @@ import React, { useCallback, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { Button } from './Button';
 import { Avatar } from './Avatar';
+import usePost from '@/hooks/usePost';
 
 interface FormProps {
   placeholder: string;
@@ -20,6 +21,7 @@ export const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) =>
 
   const { data: currentUser } = useCurrentUser();
   const { mutate: mutatePosts } = usePosts();
+  const { mutate: mutatePost } = usePost(postId as string);
 
   const [body, setBody] = useState('');
   const [isLoading, setIsLoading] = useState(false);
@@ -28,18 +30,21 @@ export const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) =>
     try {
       setIsLoading(true);
 
-      await axios.post('/api/posts', { body });
+      const url = isComment ? `/api/comments?postId=${postId}` : '/api/posts';
+
+      await axios.post(url, { body });
 
       toast.success('Tweet posted');
 
       setBody('');
       mutatePosts();
+      mutatePost();
     } catch {
       toast.error('Something went wrong');
     } finally {
       setIsLoading(false);
     }
-  }, [body, mutatePosts]);
+  }, [body, mutatePosts, mutatePost, isComment, postId]);
 
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
